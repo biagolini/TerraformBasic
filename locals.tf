c4666dcd39361c685c9e4f9df7df24935ae901dd
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-09-13 22:54:48 -0300

    Locals

---
+++ locals.tf
@@ -0,0 +1,20 @@
+locals {
+  data_aws_path = "upload/data"
+  data_myFiles_path = "myFiles/data"
+
+  img_aws_path = "upload/img"
+  img_myFiles_path = "myFiles/img"
+
+  file_names = {
+    1 = "pizza.json"
+    2 = "cat.jpeg"
+    3 = "flower.jpeg"
+  }
+
+  common_tags = {
+    Service     = "Tutorial Terraform"
+    ManagedBy   = "Terraform"
+    Environment = var.environment
+    Owner       = "Your ID"
+  }
+}
